e5bfdf68bce78675392099f0cb9aebef73d162c6
julianadenauer <github@julianadenauer.de>
2018-04-29 15:00:15 +0200

    added vision api example

---
+++ session2/06-visionapi/sketch.js
@@ -0,0 +1,43 @@
+var adresse = 'https://vision.googleapis.com/v1/images:annotate';
+var apiKey ='?key=AIzaSyC3tg8DgSol0JaCBMoyRfWFj1XhIUBpVMc';
+
+var request = {
+  "requests":[
+    {
+      "image":{
+        "source":{
+          "imageUri":
+            "http://www.zooroyal.de/magazin/wp-content/uploads/2017/06/hund-im-sommer-760x560.jpg"
+        }
+      },
+      "features":[
+        {
+          "type":"LABEL_DETECTION",
+          "maxResults":1
+        }
+      ]
+    }
+  ]
+}
+
+function setup() {
+  createCanvas(500, 500);
+
+  httpDo(
+    adresse + apiKey,
+    {
+      method: 'POST',
+      // Other Request options, like special headers for apis
+			body: JSON.stringify(request),
+    },
+    callback
+  );
+}
+
+function draw() {
+  background(150, 200, 255);
+}
+
+function callback(data) {
+  print(data);
+}
